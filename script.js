f99a9a15cf07763cda01b24f09d1472f356f5be6
Danyon <danyonstitt@gmail.com>
2022-12-06 19:04:43 +1300

    basic maths function. not linked to html elements

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// 
+function operate(a, b, operand) {
+    if(operand === "*") {return a*b;}
+    else if(operand === "+") {return a+b;}
+    else if(operand === "-") {return a-b;}
+    else if(operand === "/") {return a/b;};
+};
